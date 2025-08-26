59804689d055921e0353e7a07db6a4d6273078ce
Abhi <abhinavkonjeti@gmail.com>
2025-08-26 12:30:28 -0400

    bug fixes

--- streamlit/app.py
+++ streamlit/app.py
@@ -8,7 +8,6 @@ from model import NextWordLSTM  # Import your LSTM model class
 
 # Load tokenizer
 tokenizer = joblib.load("tokenizer.pkl")
-paradise_text = joblib.load("paradise.pkl")
 # Vocabulary size (needed for model definition)
 total_words = len(tokenizer.word_index) + 1
 
@@ -54,7 +53,8 @@ st.write("Type a sentence and choose from suggested next words to build text int
 
 # Load corpus samples
 
-corpus_text = paradise_text.split("\n")
+with open("paradise.txt", "r") as f:
+    corpus_text = f.split("\n")
 
 sample_sentences = [line for line in corpus_text if len(line.split()) > 5][:5]
 
