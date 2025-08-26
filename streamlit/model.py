cea7e9b1d2fdee02cfa85c351c331288e75d2a80
Abhi <abhinavkonjeti@gmail.com>
2025-08-26 11:52:48 -0400

    dependencies met

--- streamlit/model.py
+++ streamlit/model.py
@@ -1,6 +1,18 @@
 # Define Model
 from torch import nn
 from torch import optim
+import joblib
+
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+# Load text
+txt_file = joblib.load('paradise.pkl')
+
+# Tokenization
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts([txt_file])
+total_words = len(tokenizer.word_index) + 1
 
 class NextWordLSTM(nn.Module):
     def __init__(self, vocab_size, embed_dim, hidden_dim):
